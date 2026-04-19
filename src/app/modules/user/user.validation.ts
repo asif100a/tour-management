b1e2001514f473d15c131dde99271128c5051916
N.M. Asif Sheikh <asifsheikhrrr@gmail.com>
2026-04-19 09:30:52 +0600

    refactor(user): extract Zod schema to validation module and add middleware
    Move inline Zod schema from user.routes.ts to a new user.validation.ts file for better organization. Introduce a reusable validateRequest middleware to handle schema parsing and error forwarding, improving code reusability and separation of concerns.

---
+++ src/app/modules/user/user.validation.ts
@@ -0,0 +1,33 @@
+import z from "zod";
+
+export const createUserZodSchema = z.object({
+        name: z
+            .string({ error: "Name must be string" })
+            .min(2, { message: "The name is too short, minimum 2 characters required" })
+            .max(30, { message: "The name is too long, maximum 30 characters allowed" }),
+        email: z.email().min(5, { message: 'Email must be at least 5 characters long' }),
+        password: z
+            .string({ error: "Password must be string" })
+            .min(8, { message: "Password must be 8 characters long" })
+            .regex(/^(?=.*[A-Z])/, {
+                message: "Password must contain at least one uppercase letter",
+            })
+            .regex(/^(?=.*[@#$%^&*])/, {
+                message: "Password must contain at least one special character",
+            })
+            .regex(/^(?=.*\d)/, {
+                message: "Password must contain at least one digit",
+            })
+            .optional(),
+        phone: z
+            .string({ error: "Phone number must be string" })
+            .regex(/^(?:\+8801\d{9}|01\d{9})$/, {
+                message: "Phone number must be a Bangladeshi phone number",
+            })
+            .optional(),
+        address: z
+            .string({ error: "Address must be string" })
+            .min(2, { message: "Address must be 2 characters long" })
+            .max(100, { message: "Address too long. Maximum 100 characters allowed" })
+            .optional()
+    });
